0f5125b3b492640fa081dbc5ad88ebbc60454780
Jxanm0nt0ya <129190973+Jxanm0nt0ya@users.noreply.github.com>
2023-05-16 12:17:08 -0400

    Add files via upload

---
+++ ejercicios.js
@@ -0,0 +1,77 @@
+//numbers//
+//1- resolver o hayar el valor mayor de dos numeros
+
+/*let valor1 = prompt("Ingresa el primer numero para la comparacion: ");
+let valor2 = prompt("Ingresa el segundo numero para la comparacion: ");
+
+
+if (valor1 > valor2) {
+    alert(valor1 + " Es el mayor.")
+} else {
+    alert(valor2 + " Es el mayor.")
+}
+
+//2- sumar, restar, multiplicar y dividir  los numeros
+
+//suma//
+
+let valor3 = prompt("escribe el primer valor para la suma");
+let valor4 = prompt("escribe el segundo valor para la suma");
+let resultado_resta = (parseInt(valor3) - parseInt(valor4));
+alert("el resultado es " + resultado_resta);
+
+
+
+function sumar(a, b) {
+    console.log(a + b);
+
+}
+
+sumar(valor3, valor4)
+
+//resta (verificar me da NaN)
+let valor5 = prompt("escribe el primer valor para la resta");
+let valor6 = prompt("escribe el segundo valor para la resta");
+let resultado = (parseInt(valor5) + parseInt(valor6));
+alert("el resultado es " - resultado);
+
+function restar(a, b) {
+    console.log(a - b);
+
+}
+
+restar(valor5, valor6)
+alert("el resultado es " - resultado);*/
+
+
+
+
+
+//3- calcular la raiz cuadrada de los numeros agregados
+
+let valorCuadrado = prompt("escribe el valor para calcular la raiz");
+
+
+    Math.sqrt(valorCuadrado);
+
+alert("la raiz cuadrada es " + Math.sqrt(valorCuadrado));
+
+//string//
+//1- identificar la longitud de la cadena de texto
+//2- invertir la cadena de texto: Welcome
+//3- validar 
+
+//arrays//
+//1- agregar valores al arreglo al principio 
+//2- 
+//3- sumar todos los numeros del array (1, 2, 3)= 6
+
+//interadores ** while For//
+//1- muestrame los numeros del interador 
+//2- iterar un array de numeros y devolver el valor *2
+//3- muestrame los indicios del array que vas a iterar
+
+//Resueltos//
+
+
+
